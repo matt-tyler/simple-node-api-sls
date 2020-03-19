f195c3e8ec636d5a7157e78097f229879b1311c5
Matt Tyler <matt.tyler@mechanicalrock.io>
2020-03-19 16:25:43 +0800

    Change to REST API protected by IAM

--- client-ts/tests/api.test.ts
+++ client-ts/tests/api.test.ts
@@ -1,12 +1,46 @@
 import { DefaultApi } from "../src/api";
 import axios from "axios";
+import { createHash } from "crypto";
+import { URL } from "url"
+import { Config } from "aws-sdk"
 
+const aws4 = require('aws4');
+
+function hash(string: string) {
+    return createHash('sha256').update(string, 'utf8').digest('hex')
+}
 
 describe("Test My API: Create 3 messages", () => {
-    const instance = axios.create({
-        headers: {
-            Authorization: process.env['TOKEN']
+    const instance = axios.create()
+
+    instance.interceptors.request.use(async (config) => { 
+        const { credentials: {
+            accessKeyId, secretAccessKey
+        }} = new Config();
+
+        const url = new URL(config.url);
+        const data = config.data ? config.data : "";
+
+        const headers: { [key: string]: string }[] = [
+            { 'X-Amz-Content-Sha256': hash(data) },
+        ];
+
+        if (!new Set(['OPTIONS', 'GET']).has(config.method.toUpperCase())) {
+            headers.push({ 'Content-Type': config.headers['Content-Type'] })
         }
+    
+        const req = aws4.sign({
+            service: 'execute-api',
+            region: 'ap-southeast-2',
+            method: config.method.toUpperCase(),
+            path: `${url.pathname}${url.search}`,
+            headers: Object.assign({}, ...headers),
+            body: data,
+            host: url.host
+        }, { accessKeyId, secretAccessKey });
+
+        config.headers = req.headers;
+        return config
     })
 
     const api = new DefaultApi({},
@@ -22,7 +56,13 @@ describe("Test My API: Create 3 messages", () => {
 
     beforeEach(async (done) => {
         for (const message of messages) {
-            await api.createMessage(message);
+            try {
+                await api.createMessage(message);
+            } catch (err) {
+                console.log(err)
+                done.fail(err)
+                return;
+            }
         }
         done();
     });
@@ -30,13 +70,12 @@ describe("Test My API: Create 3 messages", () => {
     it("should return messages", async (done) => {
         const { data } = await api.listMessages(3);
         expect(data.items.length).toBe(3);
-        
         expect(data.items).toEqual(
             expect.arrayContaining(
                 messages.map(message => expect.objectContaining({
                     message,
                     author: expect.anything(),
-                    date: expect.anything()
+                    time: expect.anything()
                 }))
             ));
         done();
